91da3bed57877a108f78de4073ce5674eaf5eb28
johanneslo1 <technikclou@gmail.com>
2023-12-02 19:43:51 +0100

    initial commit

---
+++ content-script.js
@@ -0,0 +1,37 @@
+var button = document.createElement('button');
+button.innerHTML = 'Dokument herunterladen';
+button.style.backgroundColor = 'green';
+button.style.color = 'white';
+button.style.padding = '1rem 2rem';
+button.style.borderRadius = '4px';
+button.style.zIndex = '9999999';
+button.style.position = 'fixed';
+button.style.bottom = '5px';
+button.style.right = '5px';
+
+
+button.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    // Triger a keydown event.
+    // The pdf viewer of studydrive listens to the keydown event and triggers the download
+    var event = new KeyboardEvent('keydown', {
+        key: 's',
+        keyCode: 83, // numerischer Code für 's'
+        ctrlKey: true, // Strg-Taste ist gedrückt
+        altKey: false,
+        shiftKey: false,
+        metaKey: false
+    });
+
+    // Triggere das Event auf dem document-Element
+    window.dispatchEvent(event);
+
+
+});
+
+
+document.body.appendChild(button);
+
+
+
