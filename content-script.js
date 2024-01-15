2ec2eeabdb1b38d62fb3f681f6c10be29f9dc398
johanneslo1 <technikclou@gmail.com>
2024-01-15 12:16:46 +0100

    feat: restore download function in the original button

--- content-script.js
+++ content-script.js
@@ -1,38 +1,74 @@
-var button = document.createElement('button');
-button.innerHTML = 'Dokument herunterladen';
-button.style.backgroundColor = 'green';
-button.style.color = 'white';
-button.style.padding = '1rem 2rem';
-button.style.borderRadius = '4px';
-button.style.zIndex = '9999999';
-button.style.position = 'fixed';
-button.style.bottom = '5px';
-button.style.right = '5px';
 
 
-button.addEventListener('click', function (e) {
-    e.preventDefault();
+
+function downloadPdf() {
 
     // Triger a keydown event.
     // The pdf viewer of studydrive listens to the keydown event and triggers the download
     var event = new KeyboardEvent('keydown', {
         key: 's',
-        keyCode: 83, // numerischer Code für 's'
-        ctrlKey: true, // Strg-Taste ist gedrückt
+        keyCode: 83, // numerical keyCode for 's' key
+        ctrlKey: true, // ctrl key is pressed
         altKey: false,
         shiftKey: false,
         metaKey: false
     });
 
     window.dispatchEvent(event);
+}
 
+function createExtraDownloadButton() {
+    var button = document.createElement('button');
+    button.innerHTML = 'Dokument herunterladen';
+    button.style.backgroundColor = 'green';
+    button.style.color = 'white';
+    button.style.padding = '1rem 2rem';
+    button.style.borderRadius = '4px';
+    button.style.zIndex = '9999999';
+    button.style.position = 'fixed';
+    button.style.bottom = '5px';
+    button.style.right = '5px';
+
+    button.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        downloadPdf();
+    });
+
+    document.body.appendChild(button);
+}
+
+
+function changeDefaultDownloadButtonBehaviour() {
+    var originalDownloadButton = document.querySelector('button[data-specific-auth-trigger="download"]');
+
+    if (originalDownloadButton) {
+        originalDownloadButton.addEventListener('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+
+            downloadPdf();
+        }, true);
+    }
+}
+
+
+
+
+addEventListener("load", (event) => {
+
+    changeDefaultDownloadButtonBehaviour();
+
+    // Only add an extra download button if the pdf viewer is present in case the default download button is not working
+    if (document.getElementById('document-pdf-container')) {
+        createExtraDownloadButton();
+    } else {
+        console.warn('No pdf viewer found. Please write an issue on github: (https://github.com/johanneslo1/studydrive-chrome-extension/issues)');
+    }
 
 });
 
-// Only add the button if the pdf viewer is present
-if(document.getElementById('document-pdf-container')) {
-    document.body.appendChild(button);
-}
 
 
 
